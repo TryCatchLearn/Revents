c954dcf3594d35aeae57efd2e339afd03b2469be
Neil Cummings <trycatchlearn@outlook.com>
2025-03-22 18:08:21 +0700

    End of section 7

--- src/app/layout/nav/Navbar.tsx
+++ src/app/layout/nav/Navbar.tsx
@@ -1,17 +1,9 @@
-import { NavLink, useNavigate } from "react-router";
+import { Link, NavLink } from "react-router";
 import UserMenu from "./UserMenu";
-import { useAppDispatch, useAppSelector } from "../../../lib/stores/store";
-import { signIn } from "../../../features/account/accountSlice";
+import { useAppSelector } from "../../../lib/stores/store";
 
 export default function Navbar() {
-    const navigate = useNavigate();
     const user = useAppSelector(state => state.account.user);
-    const dispatch = useAppDispatch();
-
-    const handleSignIn = () => {
-        dispatch(signIn());
-        navigate('/events');
-    }
 
     return (
         <header className="px-3 w-full fixed top-0 z-50 bg-gradient-to-r from-primary to-black">
@@ -32,8 +24,8 @@ export default function Navbar() {
                         <UserMenu />
                     ) : (
                         <>
-                            <button onClick={handleSignIn} className="btn">Login</button>
-                            <button className="btn">Register</button>
+                            <Link to='/login' className="btn btn-outline btn-info">Login</Link>
+                            <button className="btn btn-outline btn-info">Register</button>
                         </>
                     )}
 
--- src/features/events/form/EventForm.tsx
+++ src/features/events/form/EventForm.tsx
@@ -3,53 +3,74 @@ import { users } from "../../../lib/data/sampleData";
 import { useAppDispatch, useAppSelector } from "../../../lib/stores/store";
 import { AppEvent } from "../../../lib/types";
 import { createEvent, selectEvent, updateEvent } from "../eventSlice";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
+import { FieldValues, useForm } from 'react-hook-form';
+import TextInput from "../../../app/shared/components/TextInput";
+import { eventFormSchema, EventFormSchema } from "../../../lib/schemas/eventFormSchema";
+import { zodResolver } from '@hookform/resolvers/zod';
+import TextArea from "../../../app/shared/components/TextArea";
+import SelectInput from "../../../app/shared/components/SelectInput";
+import { categoryOptions } from "./categoryOptions";
+import PlaceInput from "../../../app/shared/components/PlaceInput";
 
 export default function EventForm() {
   const navigate = useNavigate();
   const { id } = useParams<{ id: string }>();
   const dispatch = useAppDispatch();
   const selectedEvent = useAppSelector(state => state.event.selectedEvent);
-  const formRef = useRef<HTMLFormElement>(null);
-
-  const initialValues = selectedEvent ?? {
-    title: '',
-    category: '',
-    description: '',
-    date: '',
-    city: '',
-    venue: '',
-  }
+  const { control, handleSubmit, reset, formState: { isValid } }
+    = useForm<EventFormSchema>({
+      mode: 'onTouched',
+      resolver: zodResolver(eventFormSchema)
+    });
 
   useEffect(() => {
     if (id) {
       dispatch(selectEvent(id));
+      if (selectedEvent) {
+        reset({
+          ...selectedEvent,
+          date: new Date(selectedEvent.date).toISOString().slice(0, 16),
+          venue: {
+            venue: selectedEvent.venue,
+            latitude: selectedEvent.latitude,
+            longitude: selectedEvent.longitude,
+          }
+        })
+      }
     } else {
       dispatch(selectEvent(null));
-      formRef.current?.reset();
     }
-  }, [dispatch, id]);
+  }, [dispatch, id, selectedEvent, reset]);
 
-  const onSubmit = (formData: FormData) => {
-    const data = Object.fromEntries(formData.entries()) as unknown as AppEvent;
-
+  const onSubmit = (data: FieldValues) => {
     if (selectedEvent) {
-      dispatch(updateEvent({ ...selectedEvent, ...data }));
+      dispatch(updateEvent({ 
+        ...selectedEvent, 
+        ...data,
+        venue: data.venue.venue,
+        latitude: data.venue.latitude,
+        longitude: data.venue.longitude,
+      }));
       navigate(`/events/${selectedEvent.id}`);
       return;
     } else {
       const id = crypto.randomUUID();
-      dispatch(createEvent({
+      const newEvent = {
         ...data,
         id,
+        venue: data.venue.venue,
+        latitude: data.venue.latitude,
+        longitude: data.venue.longitude,
         hostUid: users[0].uid,
         attendees: [{
           id: users[0].uid,
           displayName: users[0].displayName,
           photoURL: users[0].photoURL,
           isHost: true,
-        }],
-      }));
+        }]
+      }
+      dispatch(createEvent(newEvent as AppEvent));
       navigate(`/events/${id}`);
     }
   }
@@ -59,43 +80,46 @@ export default function EventForm() {
       <h3 className="text-2xl font-semibold text-center text-primary">
         {selectedEvent ? 'Edit Event' : 'Create Event'}
       </h3>
-      <form ref={formRef} action={onSubmit} className="flex flex-col gap-3 w-full">
-        <input
-          defaultValue={initialValues.title}
-          name='title' type="text" className="input input-lg w-full"
-          placeholder="Event title" />
-        <input
-          defaultValue={initialValues.category}
-          name='category' type="text"
-          className="input input-lg w-full"
-          placeholder="Category" />
-        <textarea
-          defaultValue={initialValues.description}
-          name='description'
-          className="textarea textarea-lg w-full"
-          placeholder="Description" />
-        <input
-          defaultValue={initialValues.date
-            ? new Date(initialValues.date).toISOString().slice(0, 16) : ''}
-          name='date'
-          type='datetime-local'
-          className="input input-lg w-full"
-          placeholder="Date"
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6 w-full">
+        <TextInput
+          control={control}
+          name="title"
+          label="Title"
         />
-        <input
-          defaultValue={initialValues.city}
-          name="city"
-          type="text"
-          className="input input-lg w-full"
-          placeholder="City" />
-        <input
-          defaultValue={initialValues.venue}
+
+        <TextArea
+          control={control}
+          name="description"
+          label="Description"
+          rows={3}
+        />
+        <div className="flex gap-3 items-center w-full">
+          <SelectInput
+            control={control}
+            name="category"
+            label="Category"
+            options={categoryOptions}
+          />
+          <TextInput
+            control={control}
+            name="date"
+            label="Date"
+            type="datetime-local"
+            min={new Date()}
+          />
+        </div>
+
+        <PlaceInput
+          control={control}
           name="venue"
-          type="text"
-          className="input input-lg w-full" placeholder="Venue" />
+          label="Venue"
+        />
         <div className="flex justify-end w-full gap-3">
           <button onClick={() => navigate(-1)} type="button" className="btn btn-neutral">Cancel</button>
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <button
+            disabled={!isValid}
+            type="submit"
+            className="btn btn-primary">Submit</button>
         </div>
       </form>
     </div>
