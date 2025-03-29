231f003489c0380cd2e62e460e3db5d3add9b521
Neil Cummings <trycatchlearn@outlook.com>
2025-03-29 10:01:24 +0700

    End of section 12

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,15 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 import 'firebase/firestore';
 import 'firebase/auth';
-import { getAuth } from "firebase/auth";
+import 'firebase/database';
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "revents-v3-course.firebaseapp.com",
   projectId: "revents-v3-course",
+  databaseURL: "https://revents-v3-course-default-rtdb.firebaseio.com",
   storageBucket: "revents-v3-course.firebasestorage.app",
   messagingSenderId: "730293510604",
   appId: "1:730293510604:web:b95281e6cfcfbafae67d69"
@@ -20,3 +24,4 @@ export const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const fb = getDatabase(app);
